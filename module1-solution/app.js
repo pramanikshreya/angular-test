db0e7615b9d1adae03859b73827c9699115b2de8
Shreya Pramanik <pramanikshreya@gmail.com>
2016-12-10 23:41:57 +0530

    Fixed error for value updation

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -13,19 +13,21 @@ function LunchCheckController ($scope) {
 
     if(typeof $scope.lunch != 'undefined' && $scope.lunch!="") {
       $scope.spltstr = $scope.lunch.split(',');
-      $scope.length = $scope.spltstr.length;
+      console.log($scope.spltstr);
+      console.log($scope.spltstr.length);
+      if ($scope.spltstr.length <= 3 && $scope.spltstr.length >0) {
+        $scope.message = "Enjoy!";
+        $scope.success = true;
+      }
+      else if ($scope.spltstr.length > 3) {
+        $scope.message = "Too much!";
+        $scope.success = true;
+      }
     }
-
-   console.log($scope.spltstr);
-   console.log($scope.length);
-
-    if ($scope.length <= 3 && $scope.length >0)
-      $scope.message = "Enjoy!"
-    else if ($scope.length > 3)
-      $scope.message = "Too much!"
-    else
-      $scope.message = "Please enter data first"
-
+    else {
+      $scope.message = "Please enter data first";
+      $scope.success = false;
+    }
 
 
   }
